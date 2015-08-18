62ccd9ea5c2e2a66f185f6c34ef1e2ae96522a6d
Weilin Luo <william_luo2004@yahoo.com>
2015-08-17 19:26:15 -0700

    exercise 1.x

--- prog1.cc
+++ prog1.cc
@@ -2,11 +2,24 @@
 
 int main()
 {
-	std::cout << "Enter two numbers:" << std::endl;
-	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2
-		 << " is " << v1 + v2 << std::endl;
+//	std::cout << "Enter two numbers:" << std::endl;
+//	int v1 = 0, v2 = 0;
+//	std::cin >> v1 >> v2;
+	
+//	std::cout << "The sum of " << v1 << " and " << v2
+//		 << " is " << v1 + v2 << std::endl;
+
+//	std::cout << "The product of " << v1;
+//	std::cout << " and " << v2;
+//	std::cout << " is " << v1 * v2 << std::endl;
+	
+	std::cout << "/*";
+	std::cout << "*/";
+	std::cout << /* "*/" */ ";
+	std::cout << /* "*/" /* "/*" */; 
 	
 	return 0;
 }
+
+
+
