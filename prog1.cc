1ff33f051fe426e301c823649a30a779b6fc32cb
Weilin Luo <william_luo2004@yahoo.com>
2015-08-18 08:33:58 -0700

    remove *.exe files from repo.

--- prog1.cc
+++ prog1.cc
@@ -12,6 +12,8 @@ int main()
 //	std::cout << "The product of " << v1;
 //	std::cout << " and " << v2;
 //	std::cout << " is " << v1 * v2 << std::endl;
+
+	std::cout << "Hello World!" << std::endl;
 	
 	std::cout << "/*";
 	std::cout << "*/";
