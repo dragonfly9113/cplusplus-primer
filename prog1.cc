d578b934204d5e97676bda1c0a10824cd46e5b2e
Weilin Luo <william_luo2004@yahoo.com>
2015-08-18 08:58:02 -0700

    Exercises section 1.4.1

--- prog1.cc
+++ prog1.cc
@@ -13,15 +13,35 @@ int main()
 //	std::cout << " and " << v2;
 //	std::cout << " is " << v1 * v2 << std::endl;
 
-	std::cout << "Hello World!" << std::endl;
+// Exercises section 1.3 
+//	std::cout << "/*";
+//	std::cout << "*/";
+//	std::cout << /* "*/" */ ";
+//	std::cout << /* "*/" /* "/*" */; 
+
+// Exercises section 1.4.1
+	int sum = 0, cnt = 50;
+	while(cnt <= 100)	{
+		sum += cnt;
+		++cnt;
+	}
+	std::cout << "The sum from 50 to 100 is " << sum << std::endl;
 	
-	std::cout << "/*";
-	std::cout << "*/";
-	std::cout << /* "*/" */ ";
-	std::cout << /* "*/" /* "/*" */; 
+	cnt = 10;
+	while(cnt >= 0) {
+		std::cout << cnt << std::endl;
+		--cnt;
+	}
+	
+	std::cout << "Enter two numbers:" << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	
+	while(v1 <= v2) {
+		std::cout << v1 << std::endl;
+		++v1;
+	}
 	
 	return 0;
 }
-
-
 
