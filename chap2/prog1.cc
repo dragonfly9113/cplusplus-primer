e227ff8c0706d1f26bcc7d150c2f8240acf78064
Weilin Luo <william_luo2004@yahoo.com>
2015-08-30 21:57:28 -0700

    exercise 2.14

--- chap2/prog1.cc
+++ chap2/prog1.cc
@@ -18,10 +18,54 @@ int ex2_4()
 }
 
 // Exercise 2.8
-int main ()
+int ex2_8()
 {
 	std::cout << "2\115\n";
 	std::cout << "2\t\115\n";
 	
 	return 0;
 }
+
+// Exercise 2.9
+int ex2_9()
+{
+	int input_value;
+	std::cin >> input_value;
+	//int i = {3.14};
+	double salary = 9999.99, wage = salary;
+	int i = 3.14;
+	
+	std::cout << "salary = " << salary << " wage = " << wage << std::endl;
+	std::cout << "i = " << i << std::endl;
+	
+	return 0;
+}
+
+// Exercise 2.10
+std::string global_str;
+int global_int;
+
+int ex2_10()
+{
+	int local_int;
+	std::string local_str;
+	
+	std::cout << global_str << std::endl;
+	std::cout << global_int << std::endl;
+	std::cout << local_int << std::endl;
+	std::cout << local_str << std::endl;
+	
+	return 0;
+}
+
+// Exercise 2.14
+int main()
+{
+	int i = 100, sum = 0;
+	for (int i = 0; i != 10; ++i)
+		sum += i;
+	
+	std::cout << i << " " << sum << std::endl;
+}
+
+
