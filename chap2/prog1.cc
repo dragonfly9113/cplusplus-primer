9e20aad0f9e878d1203c13de36f00f82444be501
Weilin Luo <william_luo2004@yahoo.com>
2015-09-06 22:25:47 -0700

    Exercise 2.20

--- chap2/prog1.cc
+++ chap2/prog1.cc
@@ -47,7 +47,7 @@ int global_int;
 
 int ex2_10()
 {
-	int local_int;
+	int local_int = 0;
 	std::string local_str;
 	
 	std::cout << global_str << std::endl;
@@ -59,13 +59,68 @@ int ex2_10()
 }
 
 // Exercise 2.14
-int main()
+int ex2_14()
 {
 	int i = 100, sum = 0;
 	for (int i = 0; i != 10; ++i)
 		sum += i;
 	
 	std::cout << i << " " << sum << std::endl;
+	
+	return 0;
+}
+
+// Exercise 2.16
+int ex2_16()
+{
+	int i = 0, &r1 = i;
+	double d = 0, &r2 = d;
+
+	r2 = 3.14159;
+	r2 = r1;
+	i = r2;
+	r1 = d;
+	
+	return 0;
+}
+
+// Exercise 2.17
+int ex2_17()
+{
+	int i, &ri = i;
+	i = 5;
+	ri = 10;
+	
+	std::cout << i << " " << ri << std::endl;
+	
+	return 0;
+}
+
+// Exercise 2.18
+int ex2_18()
+{
+	int i1 = 10, i2 = 20;
+	int *pi1 = &i1, *pi2 = &i2;
+	
+	pi1 = &i2;
+	std::cout << *pi1 << std::endl;
+	
+	*pi2 = i1;
+	std::cout << *pi2 << std::endl;
+	
+	return 0;
+}
+
+// Exercise 2.20
+int main()
+{
+	int i = 42;
+	int *p1 = &i;
+	*p1 = *p1 * *p1;
+	
+	std::cout << i << std::endl;
+	
+	return 0;
 }
 
 
