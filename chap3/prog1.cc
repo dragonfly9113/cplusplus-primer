033a435ef9d7c45a9bca5ea74b41040d064aec69
Weilin Luo <william_luo2004@yahoo.com>
2015-09-30 11:40:22 -0700

    Exercise 3.6

--- chap3/prog1.cc
+++ chap3/prog1.cc
@@ -3,6 +3,7 @@
 #include "Sales_data.h"
 
 using std::cin; using std::cout; using std::endl; using std::cerr;
+using std::string;
 
 // Exercise 3.1: rewrite programs in exercise section 1.4.1 with appropriate using declarations
 int ex3_1_1()
@@ -33,7 +34,7 @@ int ex3_1_1()
 }
 
 // Exercise 3.1: rewrite programs in exercise section 2.6.2 with appropriate using declarations
-int main()
+int ex3_1_2()
 {
 	Sales_data total;
 	double price = 0;
@@ -74,3 +75,77 @@ int main()
 	return 0;
 }
 
+// Exercise 3.2
+int ex3_2()
+{
+	string line, word;
+	
+	// read input a line at a time
+	//while (getline(cin, line))
+	//	cout << line << endl;
+	
+	// read input a word at a time
+	while (cin >> word)
+		cout << word << endl;
+	
+	return 0;
+}
+
+// Exercise 3.4
+int ex3_4()
+{
+	string str1, str2;
+	
+	cout << "Enter the first string:" << endl;
+	getline(cin, str1);
+	cout << "Enter the second string:" << endl;
+	getline(cin, str2);
+	
+	//if (str1 == str2)
+	if (str1.size() == str2.size())
+		//cout << "str1 and str2 are equal!" << endl;
+		cout << "str1 and str2 have the same length!" << endl;
+	//else if (str1 > str2)
+	else if (str1.size() > str2.size())
+		//cout << "str1 is larger!" << endl;
+		cout << "str1 is longer!" << endl;
+	else
+		//cout << "str2 is larger!" << endl;
+		cout << "str2 is longer!" << endl;
+	
+	return 0;
+}
+
+// Exercise 3.5
+int ex3_5()
+{
+	string str, total;
+	
+	cout << "Enter several strings:" << endl;
+	// get the first line:
+	getline(cin, total);
+	
+	// read in other lines:
+	while (getline(cin, str))
+		total = total + " " + str;
+	
+	cout << total << endl;
+	
+	return 0;
+}
+
+// Exercise 3.6
+int main()
+{
+	string str;
+	
+	cout << "Enter a string:" << endl;
+	getline(cin, str);
+	
+	for (auto &c : str)
+		c = 'X';
+
+	cout << str << endl;
+	
+	return 0;
+}
