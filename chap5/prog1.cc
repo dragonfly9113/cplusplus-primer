c1c1f3cca0767be6d810aeff0a4be61a2f6239c1
Weilin Luo <william_luo2004@yahoo.com>
2015-10-10 23:31:09 -0700

    Exercise 5.7

--- chap5/prog1.cc
+++ chap5/prog1.cc
@@ -6,7 +6,7 @@ using std::cin; using std::cout; using std::endl; using std::cerr;
 using std::string; using std::vector; using std::begin; using std::end;
 
 // Exercise 5.3
-int main()
+int ex5_3()
 {
 	int sum = 0, val = 1;
 	
@@ -14,6 +14,56 @@ int main()
 		sum += val, ++val;
 	
 	cout << sum << endl;
+	
+	return 0;
+}
+
+// Exercise 5.5
+int ex5_5()
+{
+	int grade;
+	string letterGrade;
+	vector<string> letterScale{"F", "D", "C", "B", "A", "A++"};
+	
+	cout << "Enter a grade (integer): " << endl;
+	cin >> grade;
+	
+	if (grade < 60)
+		letterGrade = letterScale[0];
+	else
+		letterGrade = letterScale[(grade - 50) / 10];
+	
+	cout << letterGrade << endl;
+	
+	return 0;
+}
+
+// Exercise 5.6
+int ex5_6()
+{
+	int grade;
+	string letterGrade;
+	vector<string> letterScale{"F", "D", "C", "B", "A", "A++"};
+	
+	cout << "Enter a grade (integer): " << endl;
+	cin >> grade;
+	
+	letterGrade = (grade < 60) ? letterScale[0] : letterScale[(grade - 50) / 10];
+	
+	cout << letterGrade << endl;
+	
+	return 0;
+}
+
+// Exercise 5.7
+int main()
+{
+	int ival1 = 0, ival2 = 0;
+	
+	if (ival1 != ival2)
+		ival1 = ival2;
+	else 
+		ival1 = ival2 = 0;
 	
 	return 0;
 }
