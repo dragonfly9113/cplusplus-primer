8e75b0f777c192702104ff79b961a74a7a4226b8
Weilin Luo <william_luo2004@yahoo.com>
2015-10-11 17:15:10 -0700

    Exercise 5.11

--- chap5/prog1.cc
+++ chap5/prog1.cc
@@ -56,7 +56,7 @@ int ex5_6()
 }
 
 // Exercise 5.7
-int main()
+int ex5_7()
 {
 	int ival1 = 0, ival2 = 0;
 	
@@ -68,4 +68,74 @@ int main()
 	return 0;
 }
 
+// Exercise 5.9 and 5.10
+int ex5_10()
+{
+	unsigned aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0;
+	char ch;
+	
+	while (cin >> ch) {
+		if (ch == 'a' || ch == 'A')
+			++aCnt;
+		else if (ch == 'e' || ch == 'E')
+			++eCnt;
+		else if (ch == 'i' || ch == 'I')
+			++iCnt;
+		else if (ch == 'o' || ch == 'O')
+			++oCnt;
+		else if (ch == 'u' || ch == 'U')
+			++uCnt;
+		else
+			; // do nothing in else case.
+	}
+		
+	cout << "Number of vowel a: \t" << aCnt << '\n'
+		 << "Number of vowel e: \t" << eCnt << '\n'
+		 << "Number of vowel i: \t" << iCnt << '\n'
+		 << "Number of vowel o: \t" << oCnt << '\n'
+		 << "Number of vowel u: \t" << uCnt << endl;
+	
+	return 0;
+}
+
+// isspace example
+int example()
+{
+	char c;
+	int i = 0, sCnt = 0, tCnt = 0, nCnt = 0;
+	char str[] = "Example sentence to test		isspace\n";
+	while (str[i]) {
+		c = str[i];
+		if (c == ' ')
+			++sCnt;			
+		else if (c == '\t')
+			++tCnt;
+		else if (c == '\n')
+			++nCnt;
+		++i;
+	}
+
+	cout << sCnt << " " << tCnt << " " << nCnt << endl;
+	
+	return 0;
+}
+
+// Exercise 5.11
+int main()
+{
+	unsigned sCnt = 0;
+	char ch;
+	
+	while (cin >> ch) {
+		if (ch == ' ')
+			++sCnt;
+		
+		cout << ch;
+	}
+	
+	cout << endl;	
+	cout << "Number of spaces: \t" << sCnt << endl;
+	
+	return 0;
+}
 
